b6406f84b75006679569ad41d8311fe1afa74ce1
zanux02 <manuelbelmonteoliva@gmail.com>
2024-01-27 17:46:38 +0100

    Grabacion del humo(no se ve)

--- app/src/main/java/com/example/sutil_belmonte_grabadora/PaginaGrabar.kt
+++ app/src/main/java/com/example/sutil_belmonte_grabadora/PaginaGrabar.kt
@@ -1,11 +1,28 @@
 package com.example.sutil_belmonte_grabadora
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Button
 
 class PaginaGrabar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pagina_grabar)
+        val botongrabar: Button = findViewById(R.id.botongrabar)
+
+        botongrabar.setOnClickListener {
+            grabarVideo(it)
+        }
+    }
+    fun grabarVideo(view: View) {
+        val REQUEST_VIDEO_CAPTURE = 1
+        Intent(MediaStore.ACTION_VIDEO_CAPTURE).also { video ->
+            video.resolveActivity(packageManager)?.also {
+                startActivityForResult(video, REQUEST_VIDEO_CAPTURE)
+            }
+        }
     }
 }
