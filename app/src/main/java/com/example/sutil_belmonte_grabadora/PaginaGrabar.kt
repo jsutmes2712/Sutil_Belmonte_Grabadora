e5473277c20c1648c5fc4ea74a62fcc1c87f9c99
Juan Sutil <jsutmes2712@g.educaand.es>
2024-01-29 19:14:01 +0100

    intento de sacar url

--- app/src/main/java/com/example/sutil_belmonte_grabadora/PaginaGrabar.kt
+++ app/src/main/java/com/example/sutil_belmonte_grabadora/PaginaGrabar.kt
@@ -1,6 +1,7 @@
 package com.example.sutil_belmonte_grabadora
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -8,6 +9,9 @@ import android.view.View
 import android.widget.Button
 
 class PaginaGrabar : AppCompatActivity() {
+
+    val REQUEST_VIDEO_CAPTURE = 1
+    private var videoUri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pagina_grabar)
@@ -18,11 +22,24 @@ class PaginaGrabar : AppCompatActivity() {
         }
     }
     fun grabarVideo(view: View) {
-        val REQUEST_VIDEO_CAPTURE = 1
+
         Intent(MediaStore.ACTION_VIDEO_CAPTURE).also { video ->
             video.resolveActivity(packageManager)?.also {
                 startActivityForResult(video, REQUEST_VIDEO_CAPTURE)
             }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+            // Video grabado con éxito
+            videoUri = data?.data
+            // Ahora puedes usar videoUri para acceder a la ubicación del video grabado
+            // Por ejemplo, puedes mostrar la URL en un TextView o guardarla en una variable.
+            // Si necesitas la ruta del archivo en el dispositivo, puedes usar videoUri?.path
+
+            println(videoUri?.path)
         }
     }
 }
